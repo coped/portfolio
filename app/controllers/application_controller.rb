36233d4ec0be687efbaf5c4893ea8a9223f19ebe
Dennis Cope <coped@tutanota.com>
2019-10-17 18:27:21 -0700

    Add analytics and force SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,2 @@
 class ApplicationController < ActionController::Base
-    include Recaptcha::Adapters::ViewMethods
-    include Recaptcha::Adapters::ControllerMethods
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def full_title(title)
+        if title.empty?
+            "Dennis Cope"
+        else
+            title
+        end
+    end
 end
