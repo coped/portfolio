eb457a2157b148e61a5f9918c69b6d1bc51bd763
Dennis Cope <dennisaaroncope@gmail.com>
2020-04-21 21:59:25 -0700

    Add support for absolute module paths

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Banner from '../Banner/Banner';
-import Navbar from '../Navbar/Navbar';
-import About from '../About/About';
-import Tools from '../Tools/Tools';
-import Projects from '../Projects/Projects';
-import Contact from '../Contact/Contact';
-import Footer from '../Footer/Footer';
+import Banner from 'components/Banner/Banner';
+import Navbar from 'components/Navbar/Navbar';
+import About from 'components/About/About';
+import Tools from 'components/Tools/Tools';
+import Projects from 'components/Projects/Projects';
+import Contact from 'components/Contact/Contact';
+import Footer from 'components/Footer/Footer';
 
 function App() {
   return (
