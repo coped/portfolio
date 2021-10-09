60058f2a875c6e838241125e01bf1ebd206b95e7
Dennis Cope <dennisaaroncope@gmail.com>
2021-10-09 12:33:19 -0400

    Mock content module in Tools test

--- src/components/home/Tools.test.tsx
+++ src/components/home/Tools.test.tsx
@@ -1,35 +1,44 @@
+import type { Tool } from "types/lib/content";
 import { Tools } from "./Tools";
 import { mockTool } from "utils/testUtils";
-import { make } from "utils/utils";
-import type { Tool } from "types/lib/content";
 import { render as rtlRender, screen } from "@testing-library/react";
 
 const getToolAlt = (tool: Tool): string => tool.name + " logo";
+
+const mockTool1 = mockTool({ name: "tool1" });
+const mockTool2 = mockTool({ name: "tool2" });
+const mockTool3 = mockTool({ name: "tool3" });
+
+jest.mock("lib/content", () => ({
+  tools: {
+    backend: [{ name: "tool1" }],
+    frontend: [{ name: "tool2" }],
+    miscellaneous: [{ name: "tool3" }],
+  },
+}));
 
 describe("Tools", () => {
   it("should render without crashing", () => {
     render();
   });
 
-  // TODO: Mock tool content module
-  it.skip("Should render each tool", () => {
-    const [tool1, tool2, tool3] = make<Tool>(3).fromCallback(mockTool);
-    render({ tools: [tool1, tool2, tool3] });
+  it("Should render each tool", () => {
+    render();
 
     expect(
-      screen.getByRole("img", { name: getToolAlt(tool1) })
+      screen.getByRole("img", { name: getToolAlt(mockTool1) })
     ).toBeInTheDocument();
-    expect(screen.getByText(tool1.name)).toBeInTheDocument();
+    expect(screen.getByText(mockTool1.name)).toBeInTheDocument();
 
     expect(
-      screen.getByRole("img", { name: getToolAlt(tool2) })
+      screen.getByRole("img", { name: getToolAlt(mockTool2) })
     ).toBeInTheDocument();
-    expect(screen.getByText(tool2.name)).toBeInTheDocument();
+    expect(screen.getByText(mockTool2.name)).toBeInTheDocument();
 
     expect(
-      screen.getByRole("img", { name: getToolAlt(tool3) })
+      screen.getByRole("img", { name: getToolAlt(mockTool3) })
     ).toBeInTheDocument();
-    expect(screen.getByText(tool3.name)).toBeInTheDocument();
+    expect(screen.getByText(mockTool3.name)).toBeInTheDocument();
   });
 });
 
