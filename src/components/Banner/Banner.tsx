2e5e924ca1ef62f115965486e1a8cad46045d512
Dennis Cope <dennisaaroncope@gmail.com>
2026-05-23 02:09:21 -0400

    Update Banner.tsx

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,9 +1,7 @@
-import styled from "styled-components";
+import type { HTMLAttributes } from "react";
+import styles from "./Banner.module.css";
+import { joinCSS } from "@/lib/utils";
 
-export const Banner = styled.header`
-  display: flex;
-  background-color: #5af;
-  padding: 2em;
-  width: 100%;
-  font-size: x-large;
-`;
+export const Banner = (props: HTMLAttributes<HTMLElement>) => (
+  <header {...props} className={joinCSS(styles.banner, props.className)} />
+);
