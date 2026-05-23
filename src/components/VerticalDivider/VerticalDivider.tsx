e1569c2e86f619847e25e6660d40779a51817584
Dennis Cope <dennisaaroncope@gmail.com>
2026-05-23 12:27:40 -0400

    Refactor VerticalDivider.tsx

--- src/components/VerticalDivider/VerticalDivider.tsx
+++
@@ -1,9 +0,0 @@
-import type { HTMLAttributes } from "react";
-import "./VerticalDivider.css";
-import { joinCSS } from "@/lib/utils";
-
-export const VerticalDivider = (props: HTMLAttributes<HTMLDivElement>) => {
-  return (
-    <div {...props} className={joinCSS("vertical-divider", props.className)} />
-  );
-};
