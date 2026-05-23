73abbd2cfe8901f91d12a0fc47dc8b664cd600e2
Dennis Cope <dennisaaroncope@gmail.com>
2026-05-23 11:54:37 -0400

    Refactor Button.tsx

--- src/feature/Terminal/Button.tsx
+++ src/feature/Terminal/Button.tsx
@@ -1,16 +1,12 @@
-import styled from "styled-components";
+import { joinCSS } from "@/lib/utils";
+import type { HTMLAttributes } from "react";
 
-type Props = { color: "red" | "yellow" | "green" };
+type Props = {
+  color: "red" | "yellow" | "green";
+} & HTMLAttributes<HTMLDivElement>;
 
-export const Button = styled.div<Props>`
-  border-radius: 50%;
-  height: 0.75em;
-  width: 0.75em;
-  background-color: ${(p) => bgColorMap[p.color]};
-`;
-
-const bgColorMap = {
-  red: "#ff5f56",
-  yellow: "#ffbd2e",
-  green: "#27c93f",
+export const Button = (props: Props) => {
+  return (
+    <div {...props} className={joinCSS("button", `button--${props.color}`)} />
+  );
 };
