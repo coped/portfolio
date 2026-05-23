598a428d3ab5c53a157b77e04974cb0f578d5175
Dennis Cope <dennisaaroncope@gmail.com>
2026-05-23 16:30:27 -0400

    Refactor Copy.tsx and Banner.tsx

--- src/components/Banner/Banner.tsx
+++
@@ -1,7 +0,0 @@
-import type { HTMLAttributes } from "react";
-import styles from "./Banner.module.css";
-import { joinCSS } from "@/lib/utils";
-
-export const Banner = (props: HTMLAttributes<HTMLElement>) => (
-  <header {...props} className={joinCSS(styles.banner, props.className)} />
-);
