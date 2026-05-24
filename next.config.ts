270850a24414f48e4830136ac032e8d65c5ef52f
Dennis Cope <dennisaaroncope@gmail.com>
2026-05-23 21:39:42 -0400

    Allow S3 images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://s3.us-east-2.amazonaws.com/**",
+      ),
+    ],
+  },
 };
 
 export default nextConfig;
