8043a5505f9edc9a9b3c94e89eae64b399657428
Dennis Cope <dennisaaroncope@gmail.com>
2026-05-24 01:37:30 -0400

    Set correct use client directives

--- src/components/TerminalOutput/TerminalOutput.tsx
+++ src/components/TerminalOutput/TerminalOutput.tsx
@@ -1,11 +1,18 @@
+"use client";
 import { usePathname } from "next/navigation";
 import { Flicker } from "../Flicker/Flicker";
 import "./TerminalOutput.css";
 import { getBrowser } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
 export function TerminalOutput() {
   const pathname = usePathname();
-  const browser = getBrowser();
+  const [browser, setBrowser] = useState("");
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setBrowser(getBrowser());
+  }, []);
 
   return (
     <p className="monospace">
