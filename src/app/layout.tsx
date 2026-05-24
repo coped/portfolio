4074ef7094190234493fb1d0aae3539d60af341b
Dennis Cope <dennisaaroncope@gmail.com>
2026-05-23 23:21:05 -0400

    Fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
-import { LORA_FONT } from "@/lib/constants";
 import "./reset.css";
 import "./app.css";
+import { Lora } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Dennis Cope | cope.sh",
@@ -14,13 +14,18 @@ export const viewport: Viewport = {
   initialScale: 1,
 };
 
+const lora = Lora({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
   return (
-    <html lang="en" className={`app ${LORA_FONT.variable}`}>
+    <html lang="en" className={`app ${lora.className}`}>
       <body>{children}</body>
     </html>
   );
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,12 +7,6 @@ export const SMALL_SCREEN_MAX_WIDTH = 900;
 export const INDEX_ICON_DIMENSION = 60;
 export const INDEX_PROFILE_IMAGE_DIMENSION = 338;
 
-export const LORA_FONT = Lora({
-  weight: ["400"],
-  subsets: ["latin"],
-  variable: "--font-lora",
-});
-
 export const FOOTER_LINKS: Link[] = [
   {
     icon: github,
